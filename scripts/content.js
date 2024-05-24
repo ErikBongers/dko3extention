82eb69f6eecf9fbb5c0a214da3c157cd4ec4b63d
Erik Bongers <mail@erikbongers.net>
2024-05-24 13:39:07 +0200

    Other school: striped background.

--- scripts/content.js
+++ scripts/content.js
@@ -24,6 +24,24 @@ function db3(message) {
 
     function pageListener() {
         mainScript.onPageChanged();
+
+        let footer = document.querySelector("body > main > div.row > div.col-auto.mr-auto > small");
+        const reInstrument = /.*Je bent aangemeld als (.*)\s@\s(.*)\./;
+        const match =  footer.textContent.match(reInstrument);
+        if (match?.length !== 3) {
+            console.log(`Could not process footer text "${footer.textContent}"`);
+            return;
+        }
+        let userName = match[1];
+        let schoolName = match[2];
+        db3("user:" + userName);
+        db3("school:" + schoolName);
+        if (schoolName !== "Academie Berchem (Muziek-Woord)") {
+            document.body.classList.add("otherSchool");
+        } else {
+            document.body.classList.remove("otherSchool");
+        }
+
     }
 
     pageListener();
