c1cfee367291351f4170eabc14d841ae76278267
Erik Bongers <mail@erikbongers.net>
2024-06-19 01:05:38 +0200

    Count students in one page, per vak_leraar_graadJaar.

--- scripts/werklijst/observer.js
+++ scripts/werklijst/observer.js
@@ -4,8 +4,6 @@ import * as def from "../lessen/def.js";
 export default new HashObserver("#leerlingen-werklijst", onMutation);
 
 function onMutation(mutation) {
-    console.log(mutation);
-    console.log("WErklijst !!!");
     let werklijst = document.getElementById("div_table_werklijst");
     if (mutation.target === werklijst) {
         onWerklijstChanged(werklijst);
@@ -45,4 +43,44 @@ function addButton(targetButton, buttonId, title, clickFunction, imageId, classL
 
 function onClickFetchAll() {
     console.log("Fetching ALLLLLLL");
+    let counters = new Map();
+    fetch("/views/ui/datatable.php?id=leerlingen_werklijst&start=300&aantal=0")
+        .then((res) => {
+            res.text().then((text) => {
+                // console.log(text);
+                const template = document.createElement('template');
+                template.innerHTML = text;
+                let headers = template.content.querySelectorAll("thead th");
+
+                let headerIndices = { vak: -1, graadLeerjaar: -1, leraar: -1 };
+                for(let i = 0; i < headers.length ; i++) {
+                    switch (headers[i].textContent) {
+                        case "vak": headerIndices.vak = i; break;
+                        case "graad + leerjaar": headerIndices.graadLeerjaar = i; break;
+                        case "klasleerkracht": headerIndices.leraar = i; break;
+                    }
+                }
+                if(headerIndices.vak === -1 || headerIndices.leraar === -1 || headerIndices.graadLeerjaar === -1) {
+                    alert("ni genoeg velde");
+                    return;
+                }
+
+                let students = template.content.querySelectorAll("tbody > tr");
+                for(let student of students) {
+                    let leraar = student.children[headerIndices.leraar].textContent;
+                    if (leraar === "") leraar = "---";
+                    let keyString =
+                        student.children[headerIndices.vak].textContent + "_" +
+                        leraar + "_" +
+                        student.children[headerIndices.graadLeerjaar].textContent
+                    ;
+                    if(!counters.has(keyString)) {
+                        counters.set(keyString, { count: 0 });
+                    }
+                    counters.get(keyString).count += 1;
+                }
+                console.log("Counted " + students.length + " students.");
+                console.log(counters);
+            })
+        });
 }
