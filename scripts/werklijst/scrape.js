7f5fb90281a5b603952edd6af56726a251a92452
Erik Bongers <mail@erikbongers.net>
2024-06-19 18:44:27 +0200

    create extractStudents() in preparation of fetching all pages.

--- scripts/werklijst/scrape.js
+++ scripts/werklijst/scrape.js
@@ -4,55 +4,58 @@ export function fetchAll(doIt) {
     fetch("/views/ui/datatable.php?id=leerlingen_werklijst&start=300&aantal=0")
         .then((res) => {
             res.text().then((text) => {
-                // console.log(text);
-                const template = document.createElement('template');
-                template.innerHTML = text;
-                let headers = template.content.querySelectorAll("thead th");
-
-                let headerIndices = { vak: -1, graadLeerjaar: -1, leraar: -1 };
-                for(let i = 0; i < headers.length ; i++) {
-                    switch (headers[i].textContent) {
-                        case "vak": headerIndices.vak = i; break;
-                        case "graad + leerjaar": headerIndices.graadLeerjaar = i; break;
-                        case "klasleerkracht": headerIndices.leraar = i; break;
-                    }
-                }
-                if(headerIndices.vak === -1 || headerIndices.leraar === -1 || headerIndices.graadLeerjaar === -1) {
-                    alert("Voeg velden VAK, GRAAD+LEERJAAR en KLASLEERKRACHT toe.");
-                    return;
-                }
-
-                let students = template.content.querySelectorAll("tbody > tr");
-                for(let student of students) {
-                    let vak = student.children[headerIndices.vak].textContent;
-                    if (!isInstrument(vak)) 
-                        continue;
-                    let leraar = student.children[headerIndices.leraar].textContent;
-                    let graadLeerjaar = student.children[headerIndices.graadLeerjaar].textContent;
-                    if (leraar === "") leraar = "---";
-                    let keyString =  translateVak(vak) + "_" + leraar + "_" + graadLeerjaar;
-                    let vakLeraarKey = translateVak(vak) + "_" + leraar;
-                    if(!counters.has(keyString)) {
-                        counters.set(keyString, { count: 0 });
-                    }
-                    counters.get(keyString).count += 1;
-
-                    if(!vakLeraars.has(vakLeraarKey)) {
-                        let countMap = new Map();
-                        vakLeraars.set(vakLeraarKey, countMap);
-                    }
-                    //todo: register the count
-                    let vakLeraar = vakLeraars.get(vakLeraarKey);
-                    if(!vakLeraar.has(graadLeerjaar)) {
-                        vakLeraar.set(graadLeerjaar, {count: 0});
-                    }
-                    vakLeraars.get(vakLeraarKey).get(graadLeerjaar).count += 1;
-                }
-                console.log("Counted " + students.length + " students.");
-                console.log(vakLeraars);
+                extractStudents(text, counters, vakLeraars);
                 doIt(counters);
             })
         });
+}
+
+function extractStudents(text, counters, vakLeraars) {
+    const template = document.createElement('template');
+    template.innerHTML = text;
+    let headers = template.content.querySelectorAll("thead th");
+
+    let headerIndices = { vak: -1, graadLeerjaar: -1, leraar: -1 };
+    for(let i = 0; i < headers.length ; i++) {
+        switch (headers[i].textContent) {
+            case "vak": headerIndices.vak = i; break;
+            case "graad + leerjaar": headerIndices.graadLeerjaar = i; break;
+            case "klasleerkracht": headerIndices.leraar = i; break;
+        }
+    }
+    if(headerIndices.vak === -1 || headerIndices.leraar === -1 || headerIndices.graadLeerjaar === -1) {
+        alert("Voeg velden VAK, GRAAD+LEERJAAR en KLASLEERKRACHT toe.");
+        return;
+    }
+
+    let students = template.content.querySelectorAll("tbody > tr");
+    for(let student of students) {
+        let vak = student.children[headerIndices.vak].textContent;
+        if (!isInstrument(vak))
+            continue;
+        let leraar = student.children[headerIndices.leraar].textContent;
+        let graadLeerjaar = student.children[headerIndices.graadLeerjaar].textContent;
+        if (leraar === "") leraar = "---";
+        let keyString =  translateVak(vak) + "_" + leraar + "_" + graadLeerjaar;
+        let vakLeraarKey = translateVak(vak) + "_" + leraar;
+        if(!counters.has(keyString)) {
+            counters.set(keyString, { count: 0 });
+        }
+        counters.get(keyString).count += 1;
+
+        if(!vakLeraars.has(vakLeraarKey)) {
+            let countMap = new Map();
+            vakLeraars.set(vakLeraarKey, countMap);
+        }
+        let vakLeraar = vakLeraars.get(vakLeraarKey);
+        if(!vakLeraar.has(graadLeerjaar)) {
+            vakLeraar.set(graadLeerjaar, {count: 0});
+        }
+        vakLeraars.get(vakLeraarKey).get(graadLeerjaar).count += 1;
+    }
+    console.log("Counted " + students.length + " students.");
+    console.log(vakLeraars);
+
 }
 
 function isInstrument(vak) {
@@ -69,27 +72,6 @@ function isInstrument(vak) {
         case "Koor (musical)": 
         case "Arrangeren": 
         case "Groepsmusiceren (opera)": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
-        case "Muziekatelier": 
             return false;
     }
     return true;
