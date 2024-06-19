0fa660b5b2148f37da6ccc19196e2509832adb75
Erik Bongers <mail@erikbongers.net>
2024-06-19 22:01:13 +0200

    Prefill instruments works!

--- scripts/werklijst/observer.js
+++ scripts/werklijst/observer.js
@@ -31,6 +31,34 @@ function onPreparingFilter() {
     btnWerklijstMaken.insertAdjacentElement("beforebegin", btnPrefill);
     btnPrefill.innerText = "prefill";
     btnPrefill.id = def.PREFILL_INSTR_BTN_ID;
+    btnPrefill.onclick = prefillInstruments;
+}
+
+
+function prefillInstruments() {
+  let criteria = [
+        {"criteria":"Schooljaar","operator":"=","values":"2024-2025"},
+        {"criteria":"Status","operator":"=","values":"12"},
+        {"criteria":"Uitschrijvingen","operator":"=","values":"0"},
+        {"criteria":"Domein","operator":"=","values":"3"},
+        {"criteria":"Vak","operator":"=","values":"761,762,763,764,765,734,766,732,767,768,735,795,736,769,770,771,772,773,759,834,775,776,845,777,737,778,779,780,781,808,782,783,738,810,792,791,739,784,760,785,740,786,787,741,733,788,796,742,814,727"}
+    ];
+    sendCriteria(criteria).then(() => {
+        console.log("Crieria sent.");
+    })
+}
+
+async function sendCriteria(criteria) {
+    // Construct a FormData instance
+    const formData = new FormData();
+
+    formData.append("criteria", JSON.stringify(criteria));
+
+    const response = await fetch("/views/leerlingen/werklijst/index.criteria.session_reload.php", {
+        method: "POST",
+        body: formData,
+    });
+    // console.log(await response.text());
 }
 
 function onWerklijstChanged(tabWerklijst) {
