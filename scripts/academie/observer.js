da42694102cf589a353d6ac2ce59aa28e947e600
Erik Bongers <mail@erikbongers.net>
2024-06-22 19:59:30 +0200

    Move getUserAndSchoolName() to globals.js

--- scripts/academie/observer.js
+++ scripts/academie/observer.js
@@ -2,16 +2,20 @@ import {db3, options, PageObserver} from "../globals.js";
 
 export default new PageObserver(setSchoolBackground);
 
-function setSchoolBackground () {
+function getUserAndSchoolName() {
     let footer = document.querySelector("body > main > div.row > div.col-auto.mr-auto > small");
     const reInstrument = /.*Je bent aangemeld als (.*)\s@\s(.*)\./;
-    const match =  footer.textContent.match(reInstrument);
+    const match = footer.textContent.match(reInstrument);
     if (match?.length !== 3) {
-        console.error(`Could not process footer text "${footer.textContent}"`);
-        return;
+        throw new Error(`Could not process footer text "${footer.textContent}"`);
     }
     let userName = match[1];
     let schoolName = match[2];
+    return {userName, schoolName};
+}
+
+function setSchoolBackground () {
+    let {userName, schoolName} = getUserAndSchoolName();
     db3("user:" + userName);
     db3("school:" + schoolName);
     let isMyAcademy = options.otherAcademies
