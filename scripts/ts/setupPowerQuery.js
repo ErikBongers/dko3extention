9e800ad4b73b656a70ba27bac693f3dfd207d4a6
Erik Bongers <mail@erikbongers.net>
2024-07-06 14:20:41 +0200

    powerQuery: un-nest functions for readability.

--- scripts/ts/setupPowerQuery.js
+++ scripts/ts/setupPowerQuery.js
@@ -31,81 +31,82 @@ function scrapeMainMenu() {
     console.log(powerQueryItems);
 }
 export function setupPowerQuery() {
-    document.body.addEventListener("keydown", (ev) => {
-        if (ev.key === "q" && ev.ctrlKey && !ev.shiftKey && !ev.altKey) {
-            scrapeMainMenu();
-            popover.showPopover();
-            filterItems(searchField.textContent);
-        }
-        else {
-            if (popoverVisible === false)
-                return;
-            if (isAlphaNumeric(ev.key) || ev.key === ' ') {
-                searchField.textContent += ev.key;
-                selectedItem = 0; //back to top.
-            }
-            else if (ev.key == "Backspace") {
-                searchField.textContent = searchField.textContent.slice(0, -1);
-            }
-            else if (ev.key == "ArrowDown") {
-                selectedItem++;
-                ev.preventDefault();
-            }
-            else if (ev.key == "ArrowUp") {
-                selectedItem--;
-                ev.preventDefault();
-            }
-            else if (ev.key == "Enter") {
-                let item = powerQueryItems.find((item) => item.longLabel === list.children[selectedItem].dataset.longLabel);
-                popover.hidePopover();
-                location.href = item.href;
-                console.log(`selected: `);
-                console.log(item);
-                ev.preventDefault();
-            }
-            filterItems(searchField.textContent);
-        }
-    });
-    let popover = document.createElement("div");
-    document.querySelector("main").appendChild(popover);
-    popover.setAttribute("popover", "auto");
-    popover.id = "powerQuery";
-    popover.addEventListener("toggle", (ev) => {
-        // @ts-ignore
-        popoverVisible = ev.newState === "open";
-    });
-    let searchField = document.createElement("label");
-    popover.appendChild(searchField);
-    let list = document.createElement("div");
-    popover.appendChild(list);
-    function filterItems(needle) {
-        for (const item of powerQueryItems) {
-            item.weight = 0;
-            //exact match
-            if (item.lowerCase.includes(needle))
-                item.weight += 1000;
-            //exact match of each word in needle.
-            let needleWordsWithSeparator = needle.split(/(?= )/g);
-            if (needleWordsWithSeparator.every(word => item.lowerCase.includes(word)))
-                item.weight += 500;
-            //all chars match  in order
-            let indices = needle.split('')
-                .map(char => item.lowerCase.indexOf(char));
-            if (indices.every(num => num !== -1) && isSorted(indices))
-                item.weight += 50;
-            //all chars match
-            if (needle.split('')
-                .every(char => item.lowerCase.includes(char)))
-                item.weight += 20;
-        }
-        list.innerHTML = powerQueryItems
-            .filter((item) => item.weight != 0)
-            .sort((a, b) => b.weight - a.weight)
-            .map((item) => `<div data-long-label="${item.longLabel}">${item.weight}: ${item.longLabel}</div>`)
-            .join("\n");
-        selectedItem = clamp(selectedItem, 0, list.children.length - 1);
-        list.children[selectedItem]?.classList.add("selected");
+    //don' do nottin' - just initialize this module, below.
+}
+document.body.addEventListener("keydown", (ev) => {
+    if (ev.key === "q" && ev.ctrlKey && !ev.shiftKey && !ev.altKey) {
+        scrapeMainMenu();
+        popover.showPopover();
+        filterItems(searchField.textContent);
     }
+    else {
+        if (popoverVisible === false)
+            return;
+        if (isAlphaNumeric(ev.key) || ev.key === ' ') {
+            searchField.textContent += ev.key;
+            selectedItem = 0; //back to top.
+        }
+        else if (ev.key == "Backspace") {
+            searchField.textContent = searchField.textContent.slice(0, -1);
+        }
+        else if (ev.key == "ArrowDown") {
+            selectedItem++;
+            ev.preventDefault();
+        }
+        else if (ev.key == "ArrowUp") {
+            selectedItem--;
+            ev.preventDefault();
+        }
+        else if (ev.key == "Enter") {
+            let item = powerQueryItems.find((item) => item.longLabel === list.children[selectedItem].dataset.longLabel);
+            popover.hidePopover();
+            location.href = item.href;
+            console.log(`selected: `);
+            console.log(item);
+            ev.preventDefault();
+        }
+        filterItems(searchField.textContent);
+    }
+});
+let popover = document.createElement("div");
+document.querySelector("main").appendChild(popover);
+popover.setAttribute("popover", "auto");
+popover.id = "powerQuery";
+popover.addEventListener("toggle", (ev) => {
+    // @ts-ignore
+    popoverVisible = ev.newState === "open";
+});
+let searchField = document.createElement("label");
+popover.appendChild(searchField);
+let list = document.createElement("div");
+popover.appendChild(list);
+function filterItems(needle) {
+    for (const item of powerQueryItems) {
+        item.weight = 0;
+        //exact match
+        if (item.lowerCase.includes(needle))
+            item.weight += 1000;
+        //exact match of each word in needle.
+        let needleWordsWithSeparator = needle.split(/(?= )/g);
+        if (needleWordsWithSeparator.every(word => item.lowerCase.includes(word)))
+            item.weight += 500;
+        //all chars match  in order
+        let indices = needle.split('')
+            .map(char => item.lowerCase.indexOf(char));
+        if (indices.every(num => num !== -1) && isSorted(indices))
+            item.weight += 50;
+        //all chars match
+        if (needle.split('')
+            .every(char => item.lowerCase.includes(char)))
+            item.weight += 20;
+    }
+    list.innerHTML = powerQueryItems
+        .filter((item) => item.weight != 0)
+        .sort((a, b) => b.weight - a.weight)
+        .map((item) => `<div data-long-label="${item.longLabel}">${item.weight}: ${item.longLabel}</div>`)
+        .join("\n");
+    selectedItem = clamp(selectedItem, 0, list.children.length - 1);
+    list.children[selectedItem]?.classList.add("selected");
 }
 function isSorted(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
--- typescript/setupPowerQuery.ts
+++ typescript/setupPowerQuery.ts
@@ -45,83 +45,84 @@ function scrapeMainMenu() {
 }
 
 export function setupPowerQuery() {
+    //don' do nottin' - just initialize this module, below.
+}
 
-    document.body.addEventListener("keydown", (ev) => {
-        if (ev.key === "q" && ev.ctrlKey && !ev.shiftKey && !ev.altKey) {
-            scrapeMainMenu();
-            popover.showPopover();
-            filterItems(searchField.textContent);
-        } else {
-            if (popoverVisible === false)
-                return;
-            if (isAlphaNumeric(ev.key) || ev.key === ' ') {
-                searchField.textContent += ev.key;
-                selectedItem = 0; //back to top.
-            } else if (ev.key == "Backspace") {
-                searchField.textContent = searchField.textContent.slice(0, -1);
-            } else if (ev.key == "ArrowDown") {
-                selectedItem++;
-                ev.preventDefault();
-            } else if (ev.key == "ArrowUp") {
-                selectedItem--;
-                ev.preventDefault();
-            } else if (ev.key == "Enter") {
-                let item = powerQueryItems.find((item) => item.longLabel ===( list.children[selectedItem] as HTMLElement).dataset.longLabel);
-                popover.hidePopover();
-                location.href = item.href;
-                console.log(`selected: `);
-                console.log(item);
-                ev.preventDefault();
-            }
-            filterItems(searchField.textContent);
+document.body.addEventListener("keydown", (ev) => {
+    if (ev.key === "q" && ev.ctrlKey && !ev.shiftKey && !ev.altKey) {
+        scrapeMainMenu();
+        popover.showPopover();
+        filterItems(searchField.textContent);
+    } else {
+        if (popoverVisible === false)
+            return;
+        if (isAlphaNumeric(ev.key) || ev.key === ' ') {
+            searchField.textContent += ev.key;
+            selectedItem = 0; //back to top.
+        } else if (ev.key == "Backspace") {
+            searchField.textContent = searchField.textContent.slice(0, -1);
+        } else if (ev.key == "ArrowDown") {
+            selectedItem++;
+            ev.preventDefault();
+        } else if (ev.key == "ArrowUp") {
+            selectedItem--;
+            ev.preventDefault();
+        } else if (ev.key == "Enter") {
+            let item = powerQueryItems.find((item) => item.longLabel ===( list.children[selectedItem] as HTMLElement).dataset.longLabel);
+            popover.hidePopover();
+            location.href = item.href;
+            console.log(`selected: `);
+            console.log(item);
+            ev.preventDefault();
         }
-    })
-
-    let popover = document.createElement("div");
-    document.querySelector("main").appendChild(popover);
-    popover.setAttribute("popover", "auto");
-    popover.id = "powerQuery";
-    popover.addEventListener("toggle", (ev) => {
-        // @ts-ignore
-        popoverVisible = ev.newState === "open";
-    });
-    let searchField = document.createElement("label");
-    popover.appendChild(searchField);
-    let list = document.createElement("div");
-    popover.appendChild(list);
-
-    function filterItems(needle: string) {
-        for (const item of powerQueryItems) {
-            item.weight = 0;
-            //exact match
-            if (item.lowerCase.includes(needle))
-                item.weight += 1000;
-
-            //exact match of each word in needle.
-            let needleWordsWithSeparator = needle.split(/(?= )/g);
-            if (needleWordsWithSeparator.every(word => item.lowerCase.includes(word)))
-                item.weight += 500;
-
-            //all chars match  in order
-            let indices = needle.split('')
-                .map(char => item.lowerCase.indexOf(char));
-            if(indices.every(num => num !== -1) && isSorted(indices))
-                item.weight += 50;
-
-            //all chars match
-            if (needle.split('')
-                .every(char => item.lowerCase.includes(char)))
-                item.weight += 20;
-        }
-
-        list.innerHTML = powerQueryItems
-            .filter((item) => item.weight != 0)
-            .sort((a, b) => b.weight - a.weight)
-            .map((item) => `<div data-long-label="${item.longLabel}">${item.weight}: ${item.longLabel}</div>`)
-            .join("\n");
-        selectedItem = clamp(selectedItem, 0, list.children.length - 1);
-        list.children[selectedItem]?.classList.add("selected");
+        filterItems(searchField.textContent);
     }
+})
+
+let popover = document.createElement("div");
+document.querySelector("main").appendChild(popover);
+popover.setAttribute("popover", "auto");
+popover.id = "powerQuery";
+popover.addEventListener("toggle", (ev) => {
+    // @ts-ignore
+    popoverVisible = ev.newState === "open";
+});
+let searchField = document.createElement("label");
+popover.appendChild(searchField);
+let list = document.createElement("div");
+popover.appendChild(list);
+
+function filterItems(needle: string) {
+    for (const item of powerQueryItems) {
+        item.weight = 0;
+        //exact match
+        if (item.lowerCase.includes(needle))
+            item.weight += 1000;
+
+        //exact match of each word in needle.
+        let needleWordsWithSeparator = needle.split(/(?= )/g);
+        if (needleWordsWithSeparator.every(word => item.lowerCase.includes(word)))
+            item.weight += 500;
+
+        //all chars match  in order
+        let indices = needle.split('')
+            .map(char => item.lowerCase.indexOf(char));
+        if(indices.every(num => num !== -1) && isSorted(indices))
+            item.weight += 50;
+
+        //all chars match
+        if (needle.split('')
+            .every(char => item.lowerCase.includes(char)))
+            item.weight += 20;
+    }
+
+    list.innerHTML = powerQueryItems
+        .filter((item) => item.weight != 0)
+        .sort((a, b) => b.weight - a.weight)
+        .map((item) => `<div data-long-label="${item.longLabel}">${item.weight}: ${item.longLabel}</div>`)
+        .join("\n");
+    selectedItem = clamp(selectedItem, 0, list.children.length - 1);
+    list.children[selectedItem]?.classList.add("selected");
 }
 
 function isSorted(arr: number[]) {
