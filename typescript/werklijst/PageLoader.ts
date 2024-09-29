f3bc65d5b5ec139b15b28fcac581f49ee60cc11a
Erik Bongers <mail@erikbongers.net>
2024-09-30 01:16:00 +0200

    Test paged fetch in background.

--- typescript/werklijst/PageLoader.ts
+++ typescript/werklijst/PageLoader.ts
@@ -1,10 +1,10 @@
 import {PageHandler} from "../pageHandlers";
 import {findFirstNavigation, TableNavigation} from "../table/tableNavigation";
 
-enum PageContinue { Cancel, Continue}
-type OnPageLoadedHandler = (text: string, offset: number) => PageContinue;
-type OnLoadedHandler = () => void;
-type OnAbortHandler = () => void;
+export enum PageContinue { Cancel, Continue}
+export type OnPageLoadedHandler = (text: string, offset: number) => PageContinue;
+export type OnLoadedHandler = () => void;
+export type OnAbortHandler = (e: any) => void;
 
 export interface PageLoadHandler {
     onPage: OnPageLoadedHandler;
@@ -32,23 +32,26 @@ export class PageLoader {
                 console.log("fetching page " + offset);
                 let response = await fetch(this.buildFetchUrl(offset)); //TODO: does this need to be generic? At least have a default?
                 let text = await response.text();
-                if (this.pageLoadHandler.onPage)
-                    this.pageLoadHandler.onPage(text, offset);
+                if (this.pageLoadHandler.onPage?.(text, offset) === PageContinue.Cancel) {
+                    // noinspection ExceptionCaughtLocallyJS
+                    throw "Cancelled";
+                }
                 if(!this.navigationData) {
                     let navResponse = await fetch("https://administratie.dko3.cloud/views/ui/datatablenav.php?id="+this.pageId+"&pos=top"); //TODO: leave hard code are have buildNavUrl() callback?
                     let text = await navResponse.text();
                     let shadowTableTemplate = document.createElement("template");
                     shadowTableTemplate.innerHTML = text;
                     this.navigationData = findFirstNavigation(shadowTableTemplate.content);
+                    console.log(this.navigationData);
                 }
+                offset += this.navigationData.step;
                 if (offset > this.navigationData.maxCount) {
                     break;
                 }
-                offset += this.navigationData.step;
             }
             this.pageLoadHandler.onLoaded();
         } catch (e) {
-            this.pageLoadHandler.onAbort();
+            this.pageLoadHandler.onAbort(e);
         }
     }
 }
