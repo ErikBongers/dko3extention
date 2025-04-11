fa7185780fac21486953fba3fe591f91083cecbd
Erik Bongers <mail@erikbongers.net>
2025-04-11 21:02:24 +0200

    Removed comments.

--- typescript/serviceworker.ts
+++ typescript/serviceworker.ts
@@ -29,13 +29,9 @@ chrome.runtime.onMessage.addListener(
     function(request: ExtensionRequest, sender, sendResponse) {
         console.log("received ");
         console.log(request);
-        switch (request.action) { //todo: make a shared type with content generated.
+        switch (request.action) {
             case Actions.OpenTab:
                 let url = chrome.runtime.getURL("resources/blank.html");
-                // chrome.storage.session.set({ blank_html: request})
-                //     .then(() => {
-                //         console.log("Opening " + url);
-                //     });
                 global_request = request;
                 chrome.tabs.create({url}).then(tab => {
                     console.log("tab created.");
@@ -43,8 +39,6 @@ chrome.runtime.onMessage.addListener(
                 break;
             case Actions.GetTabData:
                 console.log("getTabData:")
-                // chrome.storage.session.get(["blank_html"]).then(result => {
-                // });
                 sendResponse(global_request);
                 break;
         }
