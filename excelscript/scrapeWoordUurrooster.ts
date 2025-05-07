1fee6641e7818769e9961df855bb430db6f89343
Erik Bongers <mail@erikbongers.net>
2025-05-08 00:54:15 +0200

    Scrape woord: scrapeColumn WIP.

--- excelscript/scrapeWoordUurrooster.ts
+++ excelscript/scrapeWoordUurrooster.ts
@@ -1,20 +1,55 @@
 /// <reference path="./excelScript.d.ts" />
 //https://github.com/sumurthy/officescripts-projects/blob/main/misc/index.d.ts
+
+type Value = string | number | boolean;
+type Data = Value[][];
+type IdxRange = {
+    start: IdxPoint,
+    end: IdxPoint
+}
+type IdxPoint = {
+    row: number,
+    column: number
+}
+
+type Time = {
+    hour: number,
+    minutes: number
+}
+
+type TimeSlice = {
+    start: Time,
+    end: Time
+}
+
+type ClassDef = {
+    day: string,
+    teacher: string,
+    timeSlice: TimeSlice,
+    description: string
+}
+
 function main(workbook: ExcelScript.Workbook) {
     let fullRange = workbook.getActiveWorksheet().getUsedRange();
     scrapeUurrooster(workbook, fullRange);
 }
 
+let data: Data = undefined;
+let daysRow: number = undefined;
+let periodColumn: number = undefined;
+let mergedRanges: IdxRange[] = undefined;
+let timeSlices: TimeSlice[] = undefined;
+
 function scrapeUurrooster(workbook: ExcelScript.Workbook, fullRange: ExcelScript.Range) {
-    let data: Data = fullRange.getValues();
-    let daysRow = findDaysRow(data);
+    data = fullRange.getValues();
+    daysRow = findDaysRow(data);
     if(daysRow === undefined)  {
         setError(workbook, "Geen rij met dagnamen gevonden.");
         return;
     } else {
         console.log("daysRow: " + daysRow);
     }
-    let periodColumn = findPeriodColumn(data);
+    periodColumn = findPeriodColumn(data);
     if(periodColumn === undefined)  {
         setError(workbook, "Geen kolom met lesmomenten gevonden.");
         return;
@@ -23,16 +58,98 @@ function scrapeUurrooster(workbook: ExcelScript.Workbook, fullRange: ExcelScript
     }
 
     // let mergedRanges = collectMergedRanges(fullRange);
-    let mergedRanges = getMergedCellsCached();
-    console.log(getCellValue(data, { row: 0, column: 1 }));
-    console.log(getCellValue(data, { row: 0, column: 2 }));
+    // console.log(JSON.stringify(mergedRanges));
+    mergedRanges = getMergedCellsCached();
+
+    timeSlices = createTimeSlices();
+    scrapeColumn(11);
 }
 
-type Value = string | number | boolean;
-type Data = Value[][];
+function scrapeColumn(column: number) {
+    let day = getCellValue({row: daysRow, column} as IdxPoint)
+    let teacher = getCellValue({ row: daysRow+1, column});
+    for(let row = daysRow+2; row < data.length; row++) {
+        let mergedRange = getMergedRangeForCell({row, column});
+        let cellValue = getCellValue({row, column})
+        if (cellValue) {
+            let timeSlice: TimeSlice = undefined;
+            if (mergedRange) {
+                let sliceStart = timeSlices[mergedRange.start.row];
+                let sliceEnd = timeSlices[mergedRange.end.row];
+                timeSlice = {start: sliceStart.start, end: sliceEnd.end};
+                row = mergedRange.end.row;
+            } else {
+                timeSlice = timeSlices[row];
+            }
+            let times = findTimes(cellValue);
+            //todo: adjust timeSlice to the found times. If only one time, use that as a starting point and keep the length of the original slice. Create function moveTimeSliceTo().
+            let classDef: ClassDef = {
+                teacher,
+                day,
+                timeSlice,
+                description: cellValue
+            };
+            console.log(JSON.stringify(classDef));
+        }
+    }
+}
 
-function getCellValue(data: Data, point: IdxPoint): string {
-    return data[point.row][point.column].toString();
+function findTimes(text: string) {
+    let times: Time[] = [];
+    let rx = /\s+(\d?\d)[.:,](\d\d)/gm;
+    let matches = rx.exec(text);
+    while(matches) {
+        let time: Time = {
+            hour: parseInt(matches[1]),
+            minutes: parseInt(matches[2])
+        };
+        times.push(time);
+        matches = rx.exec(text);
+    }
+    return times;
+}
+
+function createTimeSlices() {
+    let timeSlices: TimeSlice[] = [];
+
+    for(let row = 0; row < data.length; row++) {
+        let rx = /(\d?\d)[.:,](\d\d)\s*-\s*(\d?\d)[.:,](\d\d)/gm;
+        let value = getCellValue({row, column: periodColumn});
+        let matches = rx.exec(value);
+        if(matches) {
+            let timeSlice: TimeSlice = {
+                start: {
+                    hour: parseInt(matches[1]),
+                    minutes: parseInt(matches[2])
+                },
+                end: {
+                    hour: parseInt(matches[3]),
+                    minutes: parseInt(matches[4])
+                }
+            };
+            timeSlices.push(timeSlice);
+        } else {
+            timeSlices.push(undefined);
+        }
+    }
+    return timeSlices;
+}
+
+function getCellValue(point: IdxPoint): string {
+    let mergedRange = getMergedRangeForCell(point);
+    if(mergedRange)
+        return data[mergedRange.start.row][mergedRange.start.column].toString();
+    else
+        return data[point.row][point.column].toString();
+}
+
+function getMergedRangeForCell(point: IdxPoint): IdxRange | undefined {
+    return mergedRanges.find(range => {
+        return point.row >= range.start.row
+            && point.row <= range.end.row
+            && point.column >= range.start.column
+            && point.column <= range.end.column;
+    });
 }
 
 function findDaysRow(data: Data) {
@@ -54,15 +171,13 @@ function isDaysRow(row: Value[]) {
     return false;
 }
 
+// noinspection JSUnusedGlobalSymbols
 function collectMergedRanges(fullRange: ExcelScript.Range) {
     let mergedRanges: IdxRange [] = [];
     for (let mergedRange of fullRange.getMergedAreas().getAreas()) {
         mergedRanges.push(rangeToIndexes(mergedRange));
     }
     return mergedRanges;
-}
-
-function collectDayRanges(workbook: ExcelScript.Workbook, fullRange: ExcelScript.Range, daysRow: ExcelScript.Range, day: string) {
 }
 
 function findPeriodColumn(data: Data) {
@@ -117,21 +232,12 @@ function isDayName(text: string) {
         }
     }
 
-type IdxRange = {
-    start: IdxPoint,
-    end: IdxPoint
-}
-type IdxPoint = {
-    row: number,
-    column: number
-}
-
 function rangeToIndexes(range: ExcelScript.Range): IdxRange {
     let start: IdxPoint = { row: range.getRowIndex(), column: range.getColumnIndex() };
     let end: IdxPoint = { row: start.row + range.getRowCount()-1, column: start.column + range.getColumnCount()-1};
     return { start, end };
 }
 
-function getMergedCellsCached() {
-    return [{"start":{"row":21,"column":6},"end":{"row":24,"column":6}},{"start":{"row":16,"column":3},"end":{"row":19,"column":3}},{"start":{"row":17,"column":5},"end":{"row":18,"column":5}},{"start":{"row":21,"column":16},"end":{"row":24,"column":16}},{"start":{"row":17,"column":6},"end":{"row":20,"column":6}},{"start":{"row":23,"column":9},"end":{"row":26,"column":9}},{"start":{"row":21,"column":9},"end":{"row":22,"column":9}},{"start":{"row":19,"column":9},"end":{"row":20,"column":9}},{"start":{"row":21,"column":15},"end":{"row":26,"column":15}},{"start":{"row":21,"column":14},"end":{"row":26,"column":14}},{"start":{"row":17,"column":4},"end":{"row":18,"column":4}},{"start":{"row":21,"column":12},"end":{"row":24,"column":12}},{"start":{"row":14,"column":9},"end":{"row":15,"column":9}},{"start":{"row":15,"column":27},"end":{"row":16,"column":27}},{"start":{"row":15,"column":12},"end":{"row":16,"column":12}},{"start":{"row":17,"column":12},"end":{"row":18,"column":12}},{"start":{"row":15,"column":25},"end":{"row":16,"column":25}},{"start":{"row":15,"column":26},"end":{"row":16,"column":26}},{"start":{"row":17,"column":26},"end":{"row":18,"column":26}},{"start":{"row":16,"column":9},"end":{"row":17,"column":9}},{"start":{"row":17,"column":10},"end":{"row":18,"column":10}},{"start":{"row":15,"column":13},"end":{"row":16,"column":13}},{"start":{"row":15,"column":17},"end":{"row":16,"column":17}},{"start":{"row":17,"column":11},"end":{"row":18,"column":11}},{"start":{"row":16,"column":20},"end":{"row":19,"column":20}},{"start":{"row":21,"column":19},"end":{"row":26,"column":19}},{"start":{"row":15,"column":31},"end":{"row":16,"column":31}},{"start":{"row":15,"column":29},"end":{"row":16,"column":29}},{"start":{"row":16,"column":28},"end":{"row":17,"column":28}},{"start":{"row":15,"column":30},"end":{"row":16,"column":30}},{"start":{"row":17,"column":30},"end":{"row":18,"column":30}},{"start":{"row":16,"column":24},"end":{"row":19,"column":24}},{"start":{"row":21,"column":20},"end":{"row":23,"column":20}},{"start":{"row":20,"column":30},"end":{"row":23,"column":30}},{"start":{"row":20,"column":29},"end":{"row":23,"column":29}},{"start":{"row":17,"column":29},"end":{"row":18,"column":29}},{"start":{"row":21,"column":21},"end":{"row":24,"column":21}},{"start":{"row":24,"column":20},"end":{"row":26,"column":20}},{"start":{"row":20,"column":28},"end":{"row":23,"column":28}},{"start":{"row":21,"column":24},"end":{"row":24,"column":24}},{"start":{"row":0,"column":29},"end":{"row":0,"column":31}},{"start":{"row":10,"column":17},"end":{"row":12,"column":17}},{"start":{"row":4,"column":25},"end":{"row":13,"column":25}},{"start":{"row":0,"column":25},"end":{"row":0,"column":28}},{"start":{"row":0,"column":17},"end":{"row":0,"column":24}},{"start":{"row":10,"column":31},"end":{"row":13,"column":31}},{"start":{"row":2,"column":18},"end":{"row":4,"column":18}},{"start":{"row":10,"column":20},"end":{"row":11,"column":20}},{"start":{"row":2,"column":23},"end":{"row":5,"column":23}},{"start":{"row":2,"column":22},"end":{"row":5,"column":22}},{"start":{"row":0,"column":1},"end":{"row":0,"column":7}},{"start":{"row":23,"column":1},"end":{"row":26,"column":1}},{"start":{"row":19,"column":1},"end":{"row":22,"column":1}},{"start":{"row":20,"column":8},"end":{"row":23,"column":8}},{"start":{"row":15,"column":8},"end":{"row":16,"column":8}},{"start":{"row":21,"column":2},"end":{"row":26,"column":2}},{"start":{"row":21,"column":5},"end":{"row":24,"column":5}},{"start":{"row":17,"column":8},"end":{"row":18,"column":8}},{"start":{"row":21,"column":3},"end":{"row":26,"column":3}},{"start":{"row":15,"column":4},"end":{"row":16,"column":4}},{"start":{"row":15,"column":7},"end":{"row":16,"column":7}},{"start":{"row":0,"column":8},"end":{"row":0,"column":16}},{"start":{"row":17,"column":7},"end":{"row":18,"column":7}},{"start":{"row":10,"column":7},"end":{"row":13,"column":7}},{"start":{"row":21,"column":4},"end":{"row":24,"column":4}},{"start":{"row":19,"column":10},"end":{"row":22,"column":10}},{"start":{"row":10,"column":8},"end":{"row":13,"column":8}},{"start":{"row":7,"column":32},"end":{"row":10,"column":32}},{"start":{"row":3,"column":32},"end":{"row":4,"column":32}},{"start":{"row":5,"column":32},"end":{"row":6,"column":32}},{"start":{"row":18,"column":27},"end":{"row":21,"column":27}},{"start":{"row":14,"column":28},"end":{"row":15,"column":28}},{"start":{"row":17,"column":31},"end":{"row":18,"column":31}},{"start":{"row":15,"column":11},"end":{"row":16,"column":11}},{"start":{"row":15,"column":10},"end":{"row":16,"column":10}},{"start":{"row":4,"column":13},"end":{"row":13,"column":13}},{"start":{"row":9,"column":22},"end":{"row":10,"column":22}},{"start":{"row":11,"column":22},"end":{"row":12,"column":22}},{"start":{"row":13,"column":22},"end":{"row":14,"column":22}},{"start":{"row":12,"column":20},"end":{"row":15,"column":20}},{"start":{"row":20,"column":26},"end":{"row":23,"column":26}},{"start":{"row":22,"column":27},"end":{"row":25,"column":27}}];
+function getMergedCellsCached(): IdxRange[] {
+    return [{"start":{"row":13,"column":9},"end":{"row":16,"column":9}},{"start":{"row":10,"column":33},"end":{"row":13,"column":33}},{"start":{"row":6,"column":33},"end":{"row":7,"column":33}},{"start":{"row":8,"column":33},"end":{"row":9,"column":33}},{"start":{"row":21,"column":28},"end":{"row":24,"column":28}},{"start":{"row":17,"column":29},"end":{"row":18,"column":29}},{"start":{"row":20,"column":32},"end":{"row":21,"column":32}},{"start":{"row":18,"column":12},"end":{"row":19,"column":12}},{"start":{"row":18,"column":11},"end":{"row":19,"column":11}},{"start":{"row":7,"column":14},"end":{"row":16,"column":14}},{"start":{"row":12,"column":23},"end":{"row":13,"column":23}},{"start":{"row":14,"column":23},"end":{"row":15,"column":23}},{"start":{"row":16,"column":23},"end":{"row":17,"column":23}},{"start":{"row":15,"column":21},"end":{"row":18,"column":21}},{"start":{"row":23,"column":27},"end":{"row":26,"column":27}},{"start":{"row":25,"column":28},"end":{"row":28,"column":28}},{"start":{"row":3,"column":2},"end":{"row":3,"column":8}},{"start":{"row":26,"column":2},"end":{"row":29,"column":2}},{"start":{"row":22,"column":2},"end":{"row":25,"column":2}},{"start":{"row":23,"column":9},"end":{"row":26,"column":9}},{"start":{"row":18,"column":9},"end":{"row":19,"column":9}},{"start":{"row":24,"column":3},"end":{"row":29,"column":3}},{"start":{"row":24,"column":6},"end":{"row":27,"column":6}},{"start":{"row":20,"column":9},"end":{"row":21,"column":9}},{"start":{"row":24,"column":4},"end":{"row":29,"column":4}},{"start":{"row":18,"column":5},"end":{"row":19,"column":5}},{"start":{"row":18,"column":8},"end":{"row":19,"column":8}},{"start":{"row":3,"column":9},"end":{"row":3,"column":17}},{"start":{"row":20,"column":8},"end":{"row":21,"column":8}},{"start":{"row":13,"column":8},"end":{"row":16,"column":8}},{"start":{"row":24,"column":5},"end":{"row":27,"column":5}},{"start":{"row":22,"column":11},"end":{"row":25,"column":11}},{"start":{"row":3,"column":30},"end":{"row":3,"column":32}},{"start":{"row":13,"column":18},"end":{"row":15,"column":18}},{"start":{"row":7,"column":26},"end":{"row":16,"column":26}},{"start":{"row":3,"column":26},"end":{"row":3,"column":29}},{"start":{"row":3,"column":18},"end":{"row":3,"column":25}},{"start":{"row":13,"column":32},"end":{"row":16,"column":32}},{"start":{"row":5,"column":19},"end":{"row":7,"column":19}},{"start":{"row":13,"column":21},"end":{"row":14,"column":21}},{"start":{"row":5,"column":24},"end":{"row":8,"column":24}},{"start":{"row":5,"column":23},"end":{"row":8,"column":23}},{"start":{"row":24,"column":20},"end":{"row":29,"column":20}},{"start":{"row":18,"column":32},"end":{"row":19,"column":32}},{"start":{"row":18,"column":30},"end":{"row":19,"column":30}},{"start":{"row":19,"column":29},"end":{"row":20,"column":29}},{"start":{"row":18,"column":31},"end":{"row":19,"column":31}},{"start":{"row":20,"column":31},"end":{"row":21,"column":31}},{"start":{"row":19,"column":25},"end":{"row":22,"column":25}},{"start":{"row":24,"column":21},"end":{"row":26,"column":21}},{"start":{"row":23,"column":31},"end":{"row":26,"column":31}},{"start":{"row":23,"column":30},"end":{"row":26,"column":30}},{"start":{"row":20,"column":30},"end":{"row":21,"column":30}},{"start":{"row":24,"column":22},"end":{"row":27,"column":22}},{"start":{"row":27,"column":21},"end":{"row":29,"column":21}},{"start":{"row":23,"column":29},"end":{"row":26,"column":29}},{"start":{"row":24,"column":25},"end":{"row":27,"column":25}},{"start":{"row":17,"column":10},"end":{"row":18,"column":10}},{"start":{"row":18,"column":28},"end":{"row":19,"column":28}},{"start":{"row":18,"column":13},"end":{"row":19,"column":13}},{"start":{"row":20,"column":13},"end":{"row":21,"column":13}},{"start":{"row":18,"column":26},"end":{"row":19,"column":26}},{"start":{"row":18,"column":27},"end":{"row":19,"column":27}},{"start":{"row":20,"column":27},"end":{"row":21,"column":27}},{"start":{"row":19,"column":10},"end":{"row":20,"column":10}},{"start":{"row":20,"column":11},"end":{"row":21,"column":11}},{"start":{"row":18,"column":14},"end":{"row":19,"column":14}},{"start":{"row":18,"column":18},"end":{"row":19,"column":18}},{"start":{"row":20,"column":12},"end":{"row":21,"column":12}},{"start":{"row":19,"column":21},"end":{"row":22,"column":21}},{"start":{"row":24,"column":7},"end":{"row":27,"column":7}},{"start":{"row":19,"column":4},"end":{"row":22,"column":4}},{"start":{"row":20,"column":6},"end":{"row":21,"column":6}},{"start":{"row":24,"column":17},"end":{"row":27,"column":17}},{"start":{"row":20,"column":7},"end":{"row":23,"column":7}},{"start":{"row":26,"column":10},"end":{"row":29,"column":10}},{"start":{"row":24,"column":10},"end":{"row":25,"column":10}},{"start":{"row":22,"column":10},"end":{"row":23,"column":10}},{"start":{"row":24,"column":16},"end":{"row":29,"column":16}},{"start":{"row":24,"column":15},"end":{"row":29,"column":15}},{"start":{"row":20,"column":5},"end":{"row":21,"column":5}},{"start":{"row":24,"column":13},"end":{"row":27,"column":13}}];
 }
