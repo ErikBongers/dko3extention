429dc79eea9431ff290cb4ac40eca3a18e7c4fe9
Erik Bongers <mail@erikbongers.net>
2026-03-06 13:23:56 +0100

    Mailmerge: bug: use max length to compare cell values.

--- excelscript/PrepareMailMergeData.ts
+++ excelscript/PrepareMailMergeData.ts
@@ -3,9 +3,9 @@
 
 const TABLE_VESTING_EXTRA_INFO_ID = "tableVestigingExtraInfo" as const;
 const STUDENTEN_BASE_NAME = "Studenten" as const;
-const TABLE_STUDENTEN_ID = "table"+STUDENTEN_BASE_NAME;
+const TABLE_STUDENTEN_ID = "table" + STUDENTEN_BASE_NAME;
 const VESTIGINGSPLAATSEN_BASE_NAME = "Vestigingsplaatsen" as const;
-const TABLE_VESTIGINGSPLAATSEN_ID = "table"+VESTIGINGSPLAATSEN_BASE_NAME;
+const TABLE_VESTIGINGSPLAATSEN_ID = "table" + VESTIGINGSPLAATSEN_BASE_NAME;
 const EXTRA_INFO_COLUMN_ID = "ExtraInfo" as const;
 const VESTIGINGSPLAATS_COLUMN_ID = "Vestigingsplaats" as const;
 const DKO3DATA_SHEET_ID = "Dko3Data" as const;
@@ -68,8 +68,8 @@ function findRowIndexLargestCellValue(workbook: ExcelScript.Workbook, tableName:
     let maxIndex = -1;
     for (let rowIndex = 1; rowIndex <= range.getRowCount(); rowIndex++) {
         let cell = row.getCell(rowIndex, 0);
-        let value = cell.getValue();
-        if (value > max) {
+        let value = cell.getValue() as string;
+        if (value.length > max.length) {
             max = value as string;
             maxIndex = rowIndex;
         }
@@ -92,9 +92,9 @@ function getMetaData(workbook: ExcelScript.Workbook) {
         matchCase: true,
     });
     let metaLine = ranges.getAreas()[0];
-    let found = metaLine.find(MAX_VESTIGINGEN_PER_STUDENT_ID, {matchCase: true, completeMatch: true});
+    let found = metaLine.find(MAX_VESTIGINGEN_PER_STUDENT_ID, { matchCase: true, completeMatch: true });
     let maxVestigingenPerStudent = found.getOffsetRange(0, 1).getValue() as number;
-    return {maxVestigingenPerStudent};
+    return { maxVestigingenPerStudent };
 }
 
 function setKeyForLongestVestigingInfoInMaxRow(workbook: ExcelScript.Workbook) {
@@ -102,7 +102,7 @@ function setKeyForLongestVestigingInfoInMaxRow(workbook: ExcelScript.Workbook) {
     let firstRow = tableStudenten.getRange().getRow(1);
     let maxInfoKey = getLargestCellValueKey(workbook, TABLE_VESTING_EXTRA_INFO_ID, EXTRA_INFO_COLUMN_ID, VESTIGINGSPLAATS_COLUMN_ID);
     let metaData = getMetaData(workbook);
-    let vestiginCells = firstRow.getLastCell().getResizedRange(0, -(metaData.maxVestigingenPerStudent-1));
+    let vestiginCells = firstRow.getLastCell().getResizedRange(0, -(metaData.maxVestigingenPerStudent - 1));
     vestiginCells.setValue(maxInfoKey);
     vestiginCells.select();
 }
