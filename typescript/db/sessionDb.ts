417d6b49720d45073040d4c5d766e986b2db1c8f
ErikBongers <mail@erikbongers.net>
2026-09-10 22:44:00 +0200

    Refactor repository.ts.

--- typescript/db/sessionDb.ts
+++ typescript/db/sessionDb.ts
@@ -1,4 +1,5 @@
-import {DBSchema, IDBPDatabase, openDB, StoreKey, StoreNames, StoreValue} from 'idb';
+import {DBSchema, openDB} from 'idb';
+import {Repository} from "./repository";
 
 //todo: use typed db: https://github.com/jakearchibald/idb#examples
 
@@ -39,33 +40,6 @@ function initializeSession() {
             db.createObjectStore("Loaded");
         },
     });
-}
-
-export class Repository<Schema, K extends StoreNames<Schema>> {
-    constructor(private dbPromise: Promise<IDBPDatabase<Schema>>, private storeName: K) {
-    }
-
-    async get(id: StoreKey<Schema, K>): Promise<StoreValue<Schema, K> | undefined> {
-        return (await this.dbPromise).get(this.storeName, id);
-    }
-    async put(data: StoreValue<Schema, K>, key?: StoreKey<Schema, K>) {
-        return (await this.dbPromise).put(this.storeName, data, key);
-    }
-
-    async bulkPut(items: StoreValue<Schema, K>[]): Promise<void> {
-        let db = await this.dbPromise;
-        let tx = db.transaction(this.storeName, 'readwrite');
-        let putPromises = items.map(item => tx.store.put(item));
-
-        await Promise.all([...putPromises, tx.done]);
-    }
-
-    async findMatches(match: (item: StoreValue<Schema, K>)=> boolean) {
-        let db = await this.dbPromise;
-        let allRecords = await db.getAll(this.storeName);
-
-        return allRecords.filter(match);
-    }
 }
 
 export const SessionCache = {
